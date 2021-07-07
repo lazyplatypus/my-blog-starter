3fd452fe0437164404267b5741c2a1dfaa1e03a2
Daniel Kim <dgkim@ucdavis.edu>
2021-07-07 13:00:32 -0700

    Update constants.js

--- plugins/gatsby-plugin-newrelic/constants.js
+++ plugins/gatsby-plugin-newrelic/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  NR_KEY: '',
-  NR_LICENCE: '',
-  SITE_NAME: 'gatsby-site'
+  NR_KEY: 'process.env.NEW_RELIC_APP_NAME',
+  NR_LICENCE: 'process.env.NEW_RELIC_LICENSE_KEY',
+  SITE_NAME: 'process.env.NR_KEY'
 }
